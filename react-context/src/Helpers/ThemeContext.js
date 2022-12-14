78f58f17baa089bebac076a9774a41877ad880c8
Danek <danek.fill@outlook.com>
2022-12-14 14:30:21 -0500

    linting

--- react-context/src/Helpers/ThemeContext.js
+++ react-context/src/Helpers/ThemeContext.js
@@ -5,23 +5,20 @@ const ThemeContext = React.createContext();
 //create context for updating theme within the context of THEME and create a function lower down to use it.
 const ThemeUpdateContext =  React.createContext();
 
-//create custom hook to access theme anywhere
+//create custom hook to access theme and update value anywhere
 export function useTheme(){
   return useContext(ThemeContext)
 }
-//create custom hook to access theme update anywhere
 export function useThemeUpdate(){
   return useContext(ThemeUpdateContext)
 }
 
 export function ThemeProvider({children}){
   const [darkTheme, setDarkTheme] =  useState(true);
-
   function toggleTheme(){
     setDarkTheme(prevDarkTheme => !prevDarkTheme);
   }
 
-  //code refactored to be able to use ANY child component and give it its proper theme
   return (
     <ThemeContext.Provider value={darkTheme}>
       <ThemeUpdateContext.Provider value={toggleTheme}>
